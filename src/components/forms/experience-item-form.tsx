d86ea80eb58009261668e6f48cabe61af02e34d5
Paul2G <pggaleana@gmail.com>
2025-11-27 20:18:48 -0800

    feat: add missing fields to ExperienceItemForm

--- src/components/forms/experience-item-form.tsx
+++ src/components/forms/experience-item-form.tsx
@@ -4,6 +4,7 @@ import { useMemo } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
+import { DatePicker } from '@/components/ui/date-picker';
 import {
   Form,
   FormControl,
@@ -13,6 +14,9 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Switch } from '@/components/ui/switch';
+import { Textarea } from '@/components/ui/textarea';
+import { useFormSubmitter } from '@/hooks/use-form-submitter';
 import { useResume } from '@/hooks/use-resume';
 import { experienceItemSchema } from '@/lib/schemas';
 import { IterableSectionKey, SectionKey } from '@/lib/types';
@@ -34,9 +38,11 @@ export function ExperienceItemForm({ itemId }: ExperienceItemFormProps) {
     updateSectionDataItem(IterableSectionKey.Experience, values);
   }
 
+  useFormSubmitter(form, onSave);
+
   return (
     <Form {...form}>
-      <form onChange={form.handleSubmit(onSave)} className={'space-y-4'}>
+      <form className={'space-y-4'}>
         <FormField
           control={form.control}
           name="title"
@@ -58,6 +64,64 @@ export function ExperienceItemForm({ itemId }: ExperienceItemFormProps) {
               <FormLabel>Company name</FormLabel>
               <FormControl>
                 <Input placeholder="Microsoft" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Position activities</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Your responsabilities" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="startDate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Start date</FormLabel>
+              <FormControl>
+                <DatePicker {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="isCurrentlyWorkingHere"
+          render={({ field }) => (
+            <FormItem className="flex flex-row justify-between">
+              <FormLabel>Currently working here</FormLabel>
+              <FormControl>
+                <Switch
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="endDate"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>End date</FormLabel>
+              <FormControl>
+                <DatePicker
+                  disabled={form.getValues('isCurrentlyWorkingHere')}
+                  {...field}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
