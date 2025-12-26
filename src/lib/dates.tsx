33102b283ee55d640e1656d22dd8e454237beec7
Paul2G <pggaleana@gmail.com>
2025-12-26 02:41:13 -0800

    refactor: validate dates even if its a date object

--- src/lib/dates.tsx
+++ src/lib/dates.tsx
@@ -6,7 +6,7 @@ dayjs.extend(customParseFormat);
 const CUSTOM_DATE_FORMAT = 'DD/MM/YYYY' as const;
 
 export function parseDate(s?: string | Date): Date | undefined {
-  if (s instanceof Date) return s;
+  if (s instanceof Date) return isValidDate(s) ? s : undefined;
 
   if (!isValidStringDate(s)) return;
 
@@ -16,7 +16,8 @@ export function parseDate(s?: string | Date): Date | undefined {
 export function formatDate(d?: Date | string): string {
   if (typeof d === 'string' && isValidStringDate(d)) return d;
 
-  if (d instanceof Date) return dayjs(d).format(CUSTOM_DATE_FORMAT);
+  if (d instanceof Date && isValidDate(d))
+    return dayjs(d).format(CUSTOM_DATE_FORMAT);
 
   return '';
 }
@@ -25,4 +26,10 @@ export function isValidStringDate(s: string | undefined): boolean {
   if (!s) return false;
 
   return dayjs(s, CUSTOM_DATE_FORMAT).isValid();
+}
+
+export function isValidDate(date: Date | undefined) {
+  if (!(date instanceof Date)) return false;
+
+  return !isNaN(date.getTime());
 }
