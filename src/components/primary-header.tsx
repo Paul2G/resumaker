9dc36f5700e136a506329c979dfee5db77e03b2f
Paul2G <pggaleana@gmail.com>
2026-03-25 21:37:07 -0700

    feat: add github and portfolio links to header

--- src/components/primary-header.tsx
+++ src/components/primary-header.tsx
@@ -1,8 +1,11 @@
+import { GithubLogoIcon, GlobeIcon } from '@phosphor-icons/react';
 import { Link, useLocation } from '@tanstack/react-router';
 
 import { LocaleSelector } from '@/components/locale-selector';
 import { ResumeSelector } from '@/components/resume-selector';
 import { ThemeSelector } from '@/components/theme-selector';
+import { Button } from '@/components/ui/button';
+import { ButtonGroup } from '@/components/ui/button-group';
 
 export function PrimaryHeader() {
   const location = useLocation();
@@ -18,6 +21,26 @@ export function PrimaryHeader() {
         {location.pathname !== '/' && <ResumeSelector />}
       </div>
       <div className="ml-auto flex gap-2 items-center">
+        <ButtonGroup>
+          <Button variant="outline" size="icon" asChild>
+            <a
+              href="https://paul2g.dev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GlobeIcon />
+            </a>
+          </Button>
+          <Button variant="outline" size="icon" asChild>
+            <a
+              href="https://github.com/paul2g"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubLogoIcon />
+            </a>
+          </Button>
+        </ButtonGroup>
         <LocaleSelector />
         <ThemeSelector />
       </div>
