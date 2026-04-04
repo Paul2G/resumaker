c5f8639e5068f66aa03c4c2361d473853569008f
Paul2G <pggaleana@gmail.com>
2026-04-04 02:22:35 -0700

    feat: add button for json download

--- src/components/resume-viewer.tsx
+++ src/components/resume-viewer.tsx
@@ -1,12 +1,20 @@
 import { useRef } from 'react';
-import { PrinterIcon } from '@phosphor-icons/react';
+import { FileArrowDownIcon, PrinterIcon } from '@phosphor-icons/react';
+import { useTranslation } from 'react-i18next';
 
 import { ResumePreview } from '@/components/resume/resume-preview';
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { useResume } from '@/hooks/use-resume';
 
 export function ResumeViewer({ ...props }: ResumeViewerProps) {
+  const { t } = useTranslation();
+
   const resume = useResume();
 
   const previewRef = useRef<HTMLDivElement>(null);
@@ -17,6 +25,18 @@ export function ResumeViewer({ ...props }: ResumeViewerProps) {
     document.title = resume.config.name;
     window.print();
     document.title = pageTitle;
+  }
+
+  function downloadJSON() {
+    const dataStr =
+      'data:text/json;charset=utf-8,' +
+      encodeURIComponent(JSON.stringify(resume));
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute('href', dataStr);
+    downloadAnchorNode.setAttribute('download', `${resume.config.name}.json`);
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
   }
 
   return (
@@ -36,9 +56,32 @@ export function ResumeViewer({ ...props }: ResumeViewerProps) {
         </div>
       </ScrollArea>
       <div className="absolute bottom-4 right-4 flex flex-col gap-2">
-        <Button size="icon" onClick={printDocument}>
-          <PrinterIcon weight="light" className="size-6" />
-        </Button>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              aria-label={t('actions.downloadJson')}
+              size="icon-lg"
+              onClick={downloadJSON}
+            >
+              <FileArrowDownIcon weight="light" className="size-6" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="left">
+            {t('actions.downloadJson')}
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              aria-label={t('actions.print')}
+              size="icon-lg"
+              onClick={printDocument}
+            >
+              <PrinterIcon weight="light" className="size-6" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="left">{t('actions.print')} </TooltipContent>
+        </Tooltip>
       </div>
     </div>
   );
